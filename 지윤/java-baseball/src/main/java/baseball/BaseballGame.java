2b48148bf8dcf7646285b603660146ee00aaefe1
박지윤 <jyun1937@naver.com>
2023-05-28 14:35:01 +0900

    feat: 게임 결과 출력

--- 지윤/java-baseball/src/main/java/baseball/BaseballGame.java
+++ 지윤/java-baseball/src/main/java/baseball/BaseballGame.java
@@ -16,6 +16,17 @@ public class BaseballGame {
       System.out.print("숫자를 입력해주세요 : ");
       String[] userNumbers = Console.readLine().split("");
       matchNumbers(computerRandom,userNumbers);
+      gameResult();
+      if(strike == 3) {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int inputNextStep = Integer.parseInt(Console.readLine());
+        if(inputNextStep == 1) {
+          playGame();
+        } else {
+          break;
+        }
+      }
     }
   }
 
@@ -32,6 +43,18 @@ public class BaseballGame {
           ball += 1;
         }
       }
+    }
+  }
+
+  void gameResult() {
+    if(strike == 0 && ball > 0) {
+      System.out.printf("%d볼%n",ball);
+    } else if(ball == 0 && strike > 0) {
+      System.out.printf("%d스트라이크%n",strike);
+    } else if(ball > 0 && strike > 0) {
+      System.out.printf("%d볼 %d스트라이크%n",ball, strike);
+    } else {
+      System.out.println("낫싱");
     }
   }
 
