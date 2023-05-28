a6575286a5eeedeb99c795fac35d09d2b3ef1555
jyoung7988 <jyoung7988@naver.com>
2023-05-28 16:25:14 +0900

    Feat : 숫자 입력받기 구현

---
+++ 진영/java-baseball/src/main/java/domain/Person.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Person {
+    private List<Integer> numberList = new ArrayList<>();
+
+    public void inputStringToNumber(String str) { //987
+        for (int i = 0; i < str.length(); i++) {
+            String s = str.substring(i, i + 1);
+            Integer num = Integer.parseInt(s);
+            numberList.add(num);
+        }
+//        numberList = IntStream.range(0,str.length())
+//                .mapToObj( i -> Integer.parseInt(String.valueOf(str.charAt(i))))
+//                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getNumberList(){
+        return this.numberList;
+    }
+
+}
