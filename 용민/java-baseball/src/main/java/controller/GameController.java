1710d4f729d3b579dd7c002c45f7ddad02d0efec
YONGMIN <dragonmin25@gmail.com>
2023-05-28 16:25:22 +0900

    Feat : 숫자 입력받기 구현

---
+++ 용민/java-baseball/src/main/java/controller/GameController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import domain.Person;
+
+public class GameController {
+
+    Person person;
+     public GameController() {
+         this.person = new Person();
+     }
+     public void gameStart() {
+         System.out.print("숫자를 입력해주세요 :");
+         String str = Console.readLine();
+
+         Person person = this.person;
+         person.inputStringToNumber(str);
+     }
+
+}
