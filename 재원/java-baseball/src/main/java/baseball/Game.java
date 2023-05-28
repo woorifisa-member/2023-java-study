175e68bdb8eee3f11403c43537801d9fe12b4cf8
JaeWon <bjw1622@gmail.com>
2023-05-28 20:38:08 +0900

    feat : throw exception

--- 재원/java-baseball/src/main/java/baseball/Game.java
+++ 재원/java-baseball/src/main/java/baseball/Game.java
@@ -1,17 +1,10 @@
 package baseball;
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class Game {
 
-    public static void playGame(){
+    public static void playGame () throws IllegalArgumentException{
         boolean isNotFinshed = true;
         ArrayList<Integer> arr = makeRandomNumber();
         while (isNotFinshed){
@@ -26,6 +19,9 @@ public class Game {
                 System.out.printf("%d볼",result[1]);
                 System.out.println();
             }
+            else if(result[0] ==0 && result[1] ==0){
+                System.out.println("낫싱");
+            }
             else{
                 System.out.printf("%d볼 %d스트라이크",result[1],result[0]);
                 System.out.println();
@@ -34,31 +30,29 @@ public class Game {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 String reGame = camp.nextstep.edu.missionutils.Console.readLine();
-                if(reGame.charAt(0)==1){
+                if(reGame.charAt(0)=='1'){
                     playGame();
                 }
-                else if(reGame.charAt(0)==2){
+                else if(reGame.charAt(0)=='2'){
                     isNotFinshed=false;
                 }
             }
         }
     }
-    public static ArrayList<Integer> makeRandomNumber(){
+    public static ArrayList<Integer> makeRandomNumber() throws IllegalArgumentException{
         int firstNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(1,9);
         int secondNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(1,9);
         int thirdNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(1,9);
 
-        System.out.println(firstNumber);
-        System.out.println(secondNumber);
-        System.out.println(thirdNumber);
         ArrayList<Integer> arr = new ArrayList<>();
+
         arr.add(firstNumber);
         arr.add(secondNumber);
         arr.add(thirdNumber);
 
         return arr;
     }
-    public static Integer[] checkNumber(ArrayList<Integer> arr,String userAnswer) {
+    public static Integer[] checkNumber(ArrayList<Integer> arr,String userAnswer) throws IllegalArgumentException {
         int strike = 0;
         int ball =0;
         for(int i =0; i < userAnswer.length(); i++){
