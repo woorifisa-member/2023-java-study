8cf7976fa7ca29fd1beea79a2b37c623b14c9300
jyoung7988 <jyoung7988@naver.com>
2023-05-28 17:03:00 +0900

    Feat : 컴퓨터 랜덤 숫자 3개 생성

---
+++ 진영/java-baseball/src/main/java/domain/Computer.java
@@ -0,0 +1,25 @@
+package domain;
+
+import utils.BaseBallGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    BaseBallGenerator baseBallGenerator = new BaseBallGenerator();
+
+    private List<Integer> numberList = new ArrayList<>(); //컴퓨터 객체만의 넘버리스트
+
+    public void runGeneratorNumber(){
+        //baseBallGenerator 생성기에서 숫자를 받아올 수 있죠?
+        List<Integer> generatedList = baseBallGenerator.generateNumber();
+
+        for (Integer i : generatedList){
+            numberList.add(i);
+        }
+    }
+
+    public List<Integer> getNumberList(){
+        return numberList;
+    }
+}
