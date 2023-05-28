aeff3bf9203ed6d6863e0c84fe110921c022bc6e
박지윤 <jyun1937@naver.com>
2023-05-28 17:11:43 +0900

    fix: return 문 삭제

--- 지윤/java-baseball/src/main/java/baseball/Application.java
+++ 지윤/java-baseball/src/main/java/baseball/Application.java
@@ -8,7 +8,6 @@ public class Application {
             baseball.playGame();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
-            return;
         }
     }
 }
