b3005fc4c1686f360c34b6e19abc0df592d444cc
jyoung7988 <jyoung7988@naver.com>
2023-05-28 20:05:14 +0900

    Feat : 3스트라이크 시 출력

--- 진영/java-baseball/src/main/java/controller/GameController.java
+++ 진영/java-baseball/src/main/java/controller/GameController.java
@@ -38,6 +38,7 @@ public class GameController {
 
 
             if(resultTag){
+                System.out.println("3스트라이크");
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 boolean isStart = checkRestart();
                 if(isStart){
