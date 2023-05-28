732e81242cfefafec3f794634ca6a7618f060190
박지윤 <jyun1937@naver.com>
2023-05-28 14:39:03 +0900

    feat: Main 함수에서 BaseballGame 생성

--- 지윤/java-baseball/src/main/java/baseball/Application.java
+++ 지윤/java-baseball/src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        BaseballGame baseball = new BaseballGame();
+        try{
+            baseball.playGame();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            System.exit(0);
+        }
     }
 }
