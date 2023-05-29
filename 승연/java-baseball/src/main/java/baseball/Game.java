0ffdbda3c1981a7d5e39a5028a2fa239d86bb8f0
seungyeon gim <kjm061652@naver.com>
2023-05-29 20:27:22 +0900

    feat : 사용자 입력 예외처리 추가

--- 승연/java-baseball/src/main/java/baseball/Game.java
+++ 승연/java-baseball/src/main/java/baseball/Game.java
@@ -3,8 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.lang.IllegalArgumentException;
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -33,7 +34,25 @@ public class Game {
 
     public void gameStart(){
         System.out.print(inputMessage);
-        String input = Console.readLine();
+        String tmp = Console.readLine();
+
+        try{
+            // 문자 입력 예외처리 // 고의 에러 발생이 아닐 경우 예외처리 메시지 ?
+            int input = Integer.parseInt(tmp);
+            // 세자릿수보다 작은 수가 입력된 경우 예외처리
+            if ((int)(Math.log10(input)+1) < 3){
+                throw new IllegalArgumentException("세자릿수보다 작은 값을 입력할 수 없습니다.");
+            }
+            // 중복된 숫자가 있는 경우 예외처리
+            int[] inputIntArray = Stream.of(String.valueOf(input).split("")).mapToInt(Integer::parseInt).toArray();
+            Integer[] inputIntegerArray = Arrays.stream(inputIntArray).boxed().toArray(Integer[]::new);
+            Set<Integer> inputSet = new HashSet<Integer>(Arrays.asList(inputIntegerArray));
+            if (inputSet.size() < 3){
+                throw new IllegalArgumentException("중복된 값을 입력할 수 없습니다.");
+        }
+        }catch(IllegalArgumentException e){
+            System.out.println("에러 메세지 : " + e.getMessage());
+        }
     }
 
 
