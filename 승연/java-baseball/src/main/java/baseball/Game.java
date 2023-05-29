5e366df824c9f2d05dce55a238c944ba685270f2
seungyeon gim <kjm061652@naver.com>
2023-05-29 18:34:54 +0900

    feat : 숫자 랜덤 생성

--- 승연/java-baseball/src/main/java/baseball/Game.java
+++ 승연/java-baseball/src/main/java/baseball/Game.java
@@ -1,6 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Game {
-    // 있어야 할 기눙 : 숫자 생성, 입력받기
+    private final static int MIN_NUM = 1;
+    private final static int MAX_NUM = 9;
+    private final static int MAX_NUM_SIZE = 3;
+    ArrayList<Integer> answerNumberList = new ArrayList<Integer>();
+
+    public ArrayList<Integer> GetAnswerNumber() {
+        while (answerNumberList.size() < MAX_NUM_SIZE){
+            isInAnswerList(Randoms.pickNumberInRange(MIN_NUM, MAX_NUM));
+        }
+        return answerNumberList;
+    }
+    public void isInAnswerList(int number){
+        if (answerNumberList.contains(number)) {
+
+        }else{
+            answerNumberList.add(number);
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
+
