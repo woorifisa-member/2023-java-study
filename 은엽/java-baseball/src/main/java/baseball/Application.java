6d26ff71e73a48af40e2e5c40c42f9c4cbeb29d7
eunyeop <juju6050@naver.com>
2023-05-30 00:52:32 +0900

    Application 비교 후 받아온 result 함수로 결과값 출력 오류가 있어 수정 필요

--- 은엽/java-baseball/src/main/java/baseball/Application.java
+++ 은엽/java-baseball/src/main/java/baseball/Application.java
@@ -1,8 +1,31 @@
 package baseball;
+import Compare.NumberCompare;
+import Number.Computer;
+import Number.Human;
 
 public class Application {
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-        System.out.println("Hello World");
+        Computer computer = new Computer();
+        Human human = new Human();
+        NumberCompare numberCompare = new NumberCompare();
+        int[] computerNumbers = computer.computer();
+        int[] humanNumbers = human.getNumbers();
+        int[] results = numberCompare.compare(computerNumbers, humanNumbers);
+        System.out.println(results[0] + " " + results[1]);
     }
+
+//    public static String[] compare(int[] computerNumbers, int[] humanNumbers) {
+//        String[] results = new String[2];
+//        int count = 0;
+//        for (int i = 0; i < computerNumbers.length; i++) {
+//            if (computerNumbers[i] == humanNumbers[i]) {
+//                count++;
+//            }
+//        }
+//        results[0] = "Computer: " + count;
+//        results[1] = "Human: " + (humanNumbers.length - count);
+//        return results;
+//    }
+
 }
