ed470b31107eef0c98b5c426eca69085593cde5a
eunyeop <juju6050@naver.com>
2023-05-31 02:05:18 +0900

    실행하는 main함수

--- 은엽/java-baseball/src/main/java/baseball/Application.java
+++ 은엽/java-baseball/src/main/java/baseball/Application.java
@@ -1,31 +1,9 @@
 package baseball;
-import Compare.NumberCompare;
-import Number.Computer;
-import Number.Human;
+import Game.Result;
+
 
 public class Application {
-
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        Human human = new Human();
-        NumberCompare numberCompare = new NumberCompare();
-        int[] computerNumbers = computer.computer();
-        int[] humanNumbers = human.getNumbers();
-        int[] results = numberCompare.compare(computerNumbers, humanNumbers);
-        System.out.println(results[0] + " " + results[1]);
+        Result.result();
     }
-
-//    public static String[] compare(int[] computerNumbers, int[] humanNumbers) {
-//        String[] results = new String[2];
-//        int count = 0;
-//        for (int i = 0; i < computerNumbers.length; i++) {
-//            if (computerNumbers[i] == humanNumbers[i]) {
-//                count++;
-//            }
-//        }
-//        results[0] = "Computer: " + count;
-//        results[1] = "Human: " + (humanNumbers.length - count);
-//        return results;
-//    }
-
 }
