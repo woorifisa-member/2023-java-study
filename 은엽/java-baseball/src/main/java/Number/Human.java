9176abb65f99236e790db71237a5f053b2c8a614
eunyeop <juju6050@naver.com>
2023-05-31 02:01:42 +0900

    readLine()을 활용하여 사용자가 입력하는 값 읽어오기 숫자가 3개가 아니면 throw

--- 은엽/java-baseball/src/main/java/Number/Human.java
+++ 은엽/java-baseball/src/main/java/Number/Human.java
@@ -1,23 +1,15 @@
 package Number;
-
-import java.util.Scanner;
-import java.util.InputMismatchException;
+import camp.nextstep.edu.missionutils.Console;
 public class Human {
-    private int[] humanNumbers = new int[3];
-    public int[] getNumbers() {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("3자리의 숫자를 입력하세요.");
-            int input = scanner.nextInt();
-            if (input < 100 || input > 999) {
-                throw new IllegalArgumentException("숫자가 3자리가 아닙니다.");
-            }
-            humanNumbers[0] = input / 100;
-            humanNumbers[1] = (input % 100) / 10;
-            humanNumbers[2] = input % 10;
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다. 프로그램을 종료합니다.");
-            System.exit(0);
+    private static String[] humanNumbers = new String[3];
+    public static String[] getNumbers(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String[] input = Console.readLine().split("");
+        if (input.length != 3) {
+            throw new IllegalArgumentException("3자리 숫자만 입력해주세요");
+
+        }else{
+            humanNumbers = input;
         }
         return humanNumbers;
     }
