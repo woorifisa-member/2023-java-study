af1caed1358ddca777ec73e2829440b0ca26131c
eunyeop <juju6050@naver.com>
2023-05-31 02:03:06 +0900

    랜덤 생성한 숫자와 사용자 설정 숫자를 비교하여 strike와 ball 갯수를 계산하고 return

--- 은엽/java-baseball/src/main/java/Compare/NumberCompare.java
+++ 은엽/java-baseball/src/main/java/Compare/NumberCompare.java
@@ -1,20 +1,19 @@
 package Compare;
+
 import java.util.Arrays;
+
 public class NumberCompare {
-    public static int[] compare(int[] number1, int[] number2) {
+    public static int[] compare(int[] number1, String[] number2) {
+//        CheckBall cb = new CheckBall();
         int strike = 0;
         int ball = 0;
         int[] result = new int[2];
         for (int i = 0; i < 3; i++) {
-            if (number1[i] == number2[i]) {
-                strike++;
-            } else if (Arrays.asList(number2).contains(number1[i])) {
-                ball++;
-            }
+            strike += CheckBall.checkStrike(number1[i], number2[i]);
+            ball += CheckBall.checkBall(number1[i], number2, i);
         }
         result[0] = strike;
         result[1] = ball;
         return result;
-
     }
 }
