1016a47663973d63540f75dfdc81aa0fd611447c
박지윤 <jyun1937@naver.com>
2023-06-06 23:57:04 +0900

    feat: 당첨로또번호 입력 받기

--- 지윤/java-lotto/src/main/java/lotto/console/ControllerImpl.java
+++ 지윤/java-lotto/src/main/java/lotto/console/ControllerImpl.java
@@ -2,13 +2,16 @@ package lotto.console;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
+import lotto.io.Writer;
 import lotto.validation.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ControllerImpl implements Controller {
     @Override
     public long getPrice() {
+        Writer.print("발행할 로또의 금액을 1000원 단위로 입력해주세요 : ");
         long price = Long.parseLong(Console.readLine());
         Validator.validateInputPrice(price);
         return price / Lotto.PRICE;
@@ -16,7 +19,14 @@ public class ControllerImpl implements Controller {
 
     @Override
     public List<Integer> getWinNumber() {
-        return null;
+        List<Integer> winNumbers = new ArrayList<>();
+        Writer.print("당첨 로또 번호 6개를(1~45) 쉼표로 구분하여 입력해주세요 : ");
+        String inputNumbers = Console.readLine();
+        for(String num : inputNumbers.split(",")) {
+            int parsedNum = Integer.parseInt(num);
+            winNumbers.add(parsedNum);
+        }
+        return winNumbers;
     }
 
     @Override
