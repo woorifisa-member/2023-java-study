1eabd5c0cdc393adbbba849c16f52eff1c5e12bc
박지윤 <jyun1937@naver.com>
2023-06-06 15:17:22 +0900

    feat: 로또발행 금액 입력 예외처리 구현

--- 지윤/java-lotto/src/main/java/lotto/validation/Validator.java
+++ 지윤/java-lotto/src/main/java/lotto/validation/Validator.java
@@ -1,5 +1,7 @@
 package lotto.validation;
 
+import lotto.domain.Lotto;
+
 import java.util.List;
 
 public final class Validator {
@@ -11,6 +13,12 @@ public final class Validator {
     // TODO: 여러 검증 로직을 짜보세요
     public static void validateLottoLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateInputPrice(long price) {
+        if(price % Lotto.PRICE != 0) {
             throw new IllegalArgumentException();
         }
     }
