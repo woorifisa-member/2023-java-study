758517cdc8e92bf222903853f86801d994a0608b
박지윤 <jyun1937@naver.com>
2023-06-10 19:43:00 +0900

    feat: 로또 번호 비교 기능 구현

---
+++ 지윤/java-lotto/src/main/java/lotto/domain/CheckedLotto.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import lotto.io.Writer;
+
+public class CheckedLotto {
+
+    public void checkIssuedLotto(IssuedLotto issuedLotto, WinLotto winLotto) {
+        for (Lotto lotto : issuedLotto.getLotto()) {
+            int matchCount = compareNumbers(lotto, winLotto);
+            boolean isBonusNumber = checkBonusNumber(lotto, winLotto.getBonusNumber());
+            LottoResult.getRank(matchCount, isBonusNumber);
+        }
+    }
+
+    public void printLottoStatus() {
+        for (Rank rank : Rank.values()) {
+            int count = rank.getCount();
+            int prize = rank.getPrizeMoney();
+            Writer.print(LottoResult.formatResult(count, prize, LottoResult.getResult()[rank.ordinal()]));
+        }
+    }
+
+    private int compareNumbers(Lotto lotto, WinLotto winLotto) {
+        int matchCount = 0;
+        for (int num : winLotto.getWinLotto()) {
+            if (lotto.getNumbers().contains(num)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+    private boolean checkBonusNumber(Lotto lotto, int bonusNumber) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+}
