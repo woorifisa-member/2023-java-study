aa19670cb6f70b5f4b6dc9127d6968e7f6e26c42
jyoung7988 <jyoung7988@naver.com>
2023-06-18 21:16:43 +0900

    refactor : 피드백 반영 수정

--- 진영/java-lotto/src/main/java/lotto/validation/Validator.java
+++ 진영/java-lotto/src/main/java/lotto/validation/Validator.java
@@ -14,7 +14,7 @@ public final class Validator {
 
     // TODO: 여러 검증 로직을 짜보세요
     public static void validateLottoLength(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != Lotto.SIZE) {
             throw new IllegalArgumentException();
         }
     }
