efe0260fc15586ebde320bfb1423d6711b57c74c
Dongyeol <eastheat10@gmail.com>
2023-06-21 09:13:14 +0900

    entity mapping

--- 동열/entity-mapping/src/main/java/com/woorifisa/model/CarLocation.java
+++ 동열/entity-mapping/src/main/java/com/woorifisa/model/CarLocation.java
@@ -1,4 +1,52 @@
 package com.woorifisa.model;
 
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "car_location")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class CarLocation {
+
+    @EmbeddedId
+    private Id id;
+
+    @MapsId(value = "carId")
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @MapsId(value = "locationId")
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @Embeddable
+    @EqualsAndHashCode
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Id implements Serializable {
+        private Long carId;
+        private Long locationId;
+    }
+
+    public CarLocation(Car car, Location location) {
+        this.car = car;
+        this.location = location;
+        this.id = new Id(car.getId(), location.getId());
+    }
+
 }
--- 동열/entity-mapping/src/main/java/com/woorifisa/model/Location.java
+++ 동열/entity-mapping/src/main/java/com/woorifisa/model/Location.java
@@ -1,4 +1,43 @@
 package com.woorifisa.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "locations")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String city;
+
+    @OneToMany(mappedBy = "location")
+    private List<CarLocation> carLocations = new ArrayList<>();
+
+    @OneToMany(mappedBy = "location")
+    private List<Reserve> reserves = new ArrayList<>();
+
+    @OneToMany(mappedBy = "location")
+    private List<Return> returns = new ArrayList<>();
+
+    public Location(String city) {
+        this.city = city;
+    }
+
 }
